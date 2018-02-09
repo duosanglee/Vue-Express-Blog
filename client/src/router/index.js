2c4f57fd4c829cd7f2ea9e8fe975f73394e11fef
duosanglee <duosanglee@gmail.com>
2018-02-09 17:18:42 +0800

    路由配置完成

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,19 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import AboutMe from '@/views/AboutMe'
-import Home from '@/views/Home'
+import {routes} from './routes'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      component: Home
-    },
-    {
-      path: '/about',
-      component: AboutMe
-    }
-  ]
+  mode: 'history',
+  routes
 })
---
+++ client/src/router/routes.js
@@ -0,0 +1,50 @@
+// import Vue from 'vue'
+// import Router from 'vue-router'
+// Vue.use(Router)
+
+// const _import_ = file => () => import('views/' + file + '.vue')
+
+// const router = new Router({
+//     mode: 'history',
+//     routes: [
+//         {
+//             path: '/',
+//             component: _import_('home/index'),
+//             redirect: 'blog/all',
+//             children: [
+//                 {
+//                     path: 'blog/:classify',
+//                     component: _import_('home/blog')
+//                 }
+//             ]
+//         },
+//         {
+//             path: '/article/:id',
+//             component: _import_('Article/index')
+//         }
+//     ]
+// })
+
+// export default
+import AboutMe from '@/views/AboutMe'
+import Home from '@/views/Home'
+
+export const routes = [
+  {
+    path: '/',
+    redirect: '/home',
+    name: 'home',
+    component: Home,
+    meta: {
+      title: '首页'
+    }
+  },
+  {
+    path: '/aboutMe',
+    component: AboutMe,
+    name: 'aboutMe',
+    meta: {
+      title: '关于我'
+    }
+  }
+]
