6932823b0f44ff2814864e99e138ae236ab828bd
duosanglee <duosanglee@gmail.com>
2018-02-26 13:45:14 +0800

    修改http层配置

--- client/src/config/http.js
+++ client/src/config/http.js
@@ -19,33 +19,10 @@ axios.interceptors.request.use(
   }
 )
 
-// const filters = [
-//   '/user/addMobile',
-//   '/login',
-//   '/regist',
-//   '/user/validateOldMobile',
-//   '/discuss/get', // 讨论轮询
-//   '/privateLetter/get', // 私信轮询
-//   '/work/getUserUnRatingWork'
-// ]
-
 // http response 拦截器
 axios.interceptors.response.use(
   response => {
-    // store.commit('app/setAuth', response.data)
-    // if (response.data.code === 1101) router.push('/login') // 没有登录，去登陆
-    // let length = filters.length
-    // for (let i = 0; i < length; i++) {
-    //   if (response.config.url.indexOf(filters[i]) !== -1) {
-    //     return response.data
-    //   }
-    // }
-    // if (response.data.code && response.data.code !== 1000) {
-    //   alert(response.data.data)
-    //   return Promise.reject(response.data.data)
-    // } else {
     return response.data
-    // }
   },
   error => {
     alert(error.response.data)
@@ -53,56 +30,23 @@ axios.interceptors.response.use(
   }
 )
 
-let post = (url, data, {
-  onUploadProgressCal
-} = {}) => {
+let post = (url, data) => {
   return axios({
     method: 'post',
     url,
-    data: qs.stringify(data),
-    onUploadProgress: function (progressEvent) {
-      // 对原生进度事件的处理
-      if (onUploadProgressCal) {
-        onUploadProgressCal(progressEvent)
-      }
-    }
+    data: qs.stringify(data)
   })
 }
 
-let get = (url, params, {
-  onUploadProgressCal
-} = {}) => {
+let get = (url, params) => {
   return axios({
     method: 'get',
     url,
-    params,
-    onUploadProgress: function (progressEvent) {
-      // 对原生进度事件的处理
-      if (onUploadProgressCal) {
-        onUploadProgressCal(progressEvent)
-      }
-    }
-  })
-}
-
-let put = (url, data, {
-  onUploadProgressCal
-} = {}) => {
-  return axios({
-    method: 'put',
-    url,
-    data: data,
-    onUploadProgress: function (progressEvent) {
-      // 对原生进度事件的处理
-      if (onUploadProgressCal) {
-        onUploadProgressCal(progressEvent)
-      }
-    }
+    params
   })
 }
 
 export default {
   post,
-  get,
-  put
+  get
 }
