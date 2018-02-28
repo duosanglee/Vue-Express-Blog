f0bba47ee694bd3fc90c0a4e9aad5a84d3cc14ca
duosanglee <duosanglee@gmail.com>
2018-02-28 16:13:09 +0800

    首页导航更改

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <div class="nav-wrap">
-      <router-link :to="{name: route.name}" class="nav" v-for="(route, key) of route" :key="key">{{route.meta.title}}</router-link>
+      <router-link :to="{name: route.name}" class="nav" v-for="(route, key) of link" :key="key">{{route.title}}</router-link>
     </div>
     <router-view class="main"></router-view>
   </div>
@@ -12,7 +12,16 @@ export default {
   name: 'App',
   data () {
     return {
-
+      link: [
+        {
+          name: 'home',
+          title: '首页'
+        },
+        {
+          name: 'aboutMe',
+          title: '关于我'
+        }
+      ]
     }
   },
   computed: {
@@ -31,16 +40,22 @@ export default {
 
 <style lang="scss" scoped>
 .nav-wrap {
+  position: absolute;
+  left: 50%;
+  transform: translateX(-50%);
+  top: 0;
   display: flex;
   justify-content: space-between;
-  margin: 0 auto;
   padding: 0 20px;
+  width: 100%;
   max-width: 1000px;
   height: 60px;
   line-height: 60px;
+  z-index: 100;
   .nav {
-    font-size: 14px;
-    color: rgba(31, 31, 31, 0.5);
+    font-size: 16px;
+    color: rgb(31, 31, 31);
+    font-weight: normal;
     &.router-link-active {
       font-size: 26px;
       font-weight: bold;
