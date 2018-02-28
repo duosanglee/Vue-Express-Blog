8bfedcd0abed979cde232336b4eec983b69959ce
duosanglee <duosanglee@gmail.com>
2018-02-28 11:12:59 +0800

    主页更改

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,28 +1,52 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <div class="nav-wrap">
+      <router-link :to="{name: route.name}" class="nav" v-for="(route, key) of route" :key="key">{{route.meta.title}}</router-link>
+    </div>
+    <router-view class="main"></router-view>
   </div>
 </template>
 
 <script>
 export default {
   name: 'App',
+  data () {
+    return {
+
+    }
+  },
+  computed: {
+    route () {
+      return this.$router.options.routes.slice(1)
+    }
+  },
   created () {
-    this.$api.test().then(data => {
-      console.log(data)
-    })
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import "./style/reset.css";
+</style>
+
+<style lang="scss" scoped>
+.nav-wrap {
+  display: flex;
+  justify-content: space-between;
+  margin: 0 auto;
+  padding: 0 20px;
+  max-width: 1000px;
+  height: 60px;
+  line-height: 60px;
+  .nav {
+    font-size: 14px;
+    color: rgba(31, 31, 31, 0.5);
+    &.router-link-active {
+      font-size: 26px;
+      font-weight: bold;
+      color: #222;
+      order: -1;
+    }
+  }
 }
 </style>
